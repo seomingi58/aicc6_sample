e6afdd7718179505b22e4a85ff6e6683c17252a8
seomingi58 <alswlalswl1592@gamil.com>
2025-05-02 12:17:33 +0900

    0502

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,39 @@
-//  menu toggle
-const menu = document.querySelector(".menu");
-const icon = document.querySelector(".menu i");
+// menu toggle
+const menu = document.querySelector('.menu');
+const icon = document.querySelector('.menu i');
+const nav = document.querySelector('.navi');
 
-// menu를 클릭하면 acttive 라는 이름의 클래스가 생성 됐다 안됐다 한다. (toggle)
+// menu를 클릭하면 active라는 이름의 클래스가 생성 됐다 안됐다 한다.(toggle)
+menu.addEventListener('click', function () {
+  this.classList.toggle('active');
+  const navHeight = nav.scrollHeight;
 
-menu.addEventListener("click", function(){
-    this.classList.toggle("active");
+  console.log(navHeight);
 
-//    if(this.classList.contains('active')){
-//        console.log("있다");
-//       icon.setAttribute("class", "ri-close-line");
-//    } else {
-//        console.log("없다");
-//      icon.setAttribute("class","ri-menu-line");
-//    }
+  if (this.classList.contains('active')) {
+    icon.setAttribute('class', 'ri-close-line');
+    nav.style.height = navHeight + 'px';
+  } else {
+    icon.setAttribute('class', 'ri-menu-line');
+    nav.style.height = 0;
+  }
 
-    this.classList.contains('active') ?  icon.setAttribute("class", "ri-close-line") : icon.setAttribute("class","ri-menu-line");
-    
-})
+  //   this.classList.contains('active')
+  //     ? icon.setAttribute('class', 'ri-close-line')
+  //     : icon.setAttribute('class', 'ri-menu-line');
+});
 
-console.log(menu);
+// Navigation Activate
+const navs = document.querySelectorAll('.navi li');
+
+navs.forEach((nav, idx) => {
+  nav.addEventListener('click', function () {
+    navs.forEach((nav) => {
+      nav.classList.remove('active');
+    });
+
+    this.classList.add('active');
+  });
+});
+
+// console.log(navs);
